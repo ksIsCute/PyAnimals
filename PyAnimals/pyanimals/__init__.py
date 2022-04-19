d016fca1eaff123cb204f63099e6ab09e51edea0
nullfact0r <82207258+nullfact0r@users.noreply.github.com>
2022-04-19 10:13:58 +0200

    Refactor main class, DRY (don't repeat yourself)

--- PyAnimals/pyanimals/__init__.py
+++ PyAnimals/pyanimals/__init__.py
@@ -1,18 +1,23 @@
 import requests
 
 __version__ = '0.1.0'
-
-class animal:
-    def fact(animal):
-        if animal.lower() not in ["dog", "cat", "raccoon", "panda", "kangaroo", "koala", "fox", "bird"]:
-            raise AttributeError("The animal given doesnt exist! Make sure the animal you want an fact of is in the respected queries!")
         
-        animallink = requests.get(f"https://some-random-api.ml/animal/{animal}")
-        animaljson = animallink.json()
-        return animaljson['fact']
+class animal:
+    animals = ["dog", "cat", "raccoon", "panda", "kangaroo", "koala", "fox", "bird"]
+    
+    def __check_animal(animal):
+        if animal.lower() not in animals:
+            raise AttributeError("The animal given doesnt exist! Make sure the animal you want an fact of is in the respected queries!")
+            
+    def __get_animal(animal):
+        self.__check_animal(animal)
+        
+        link = requests.get(f"https://some-random-api.ml/animal/{animal}")
+        json = link.json()
+        return json
+        
+    def fact(animal):
+        return self.__get_animal(animal)["fact"]
+    
     def picture(animal: str):
-        if animal.lower() not in ["dog", "cat", "raccoon", "panda", "kangaroo", "koala", "fox", "bird"]:
-            raise AttributeError("The animal given doesnt exist! Make sure the animal you want an image of is in the respected queries!")
-        animallink = requests.get(f"https://some-random-api.ml/animal/{animal}")
-        animaljson = animallink.json()
-        return animaljson['image']
+        return self.__get_animal(animal)['image']
